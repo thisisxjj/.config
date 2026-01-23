4ed078ac58e4f4d61f74a55a88f95e20e56c36be
thisisxjj <763801840@qq.com>
2026-01-24 00:15:16 +0800

    reset v mod keymap

--- zsh/vi.zsh
+++ zsh/vi.zsh
@@ -1,17 +1,5 @@
 bindkey '^v' edit-command-line
 bindkey -v
-bindkey -M vicmd "n" vi-insert               # n 进入插入模式
-bindkey -M vicmd "N" vi-insert-bol
-bindkey -M vicmd "j" vi-backward-char
-bindkey -M vicmd "l" vi-forward-char
-bindkey -M vicmd "J" vi-beginning-of-line
-bindkey -M vicmd "L" vi-end-of-line
-bindkey -M vicmd "k" down-line-or-history
-bindkey -M vicmd "i" up-line-or-history
-bindkey -M vicmd "u" undo
-#bindkey -M vicmd "-" vi-rev-repeat-search
-bindkey -M vicmd "=" vi-repeat-search
-#bindkey -M vicmd "h" vi-forward-word-end
 
 function zle-keymap-select {
 	if [[ ${KEYMAP} == vicmd ]] || [[ $1 = 'block' ]]; then
