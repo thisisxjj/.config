0ae6e9b53ebe959a9be93ee6ec30d163b11956e8
thisisxjj <763801840@qq.com>
2026-03-04 13:19:25 +0800

    nvim gitsigns config change

--- nvim/lua/plugins/editor/gitsigns.lua
+++ nvim/lua/plugins/editor/gitsigns.lua
@@ -2,22 +2,6 @@ return {
 	"lewis6991/gitsigns.nvim",
 	event = { "BufReadPre", "BufNewFile" },
 	opts = {
-		signs = {
-			add = { text = "󰾗 " },
-			change = { text = "󰯔 " },
-			delete = { text = "_" },
-			topdelete = { text = "‾" },
-			changedelete = { text = "~" },
-			untracked = { text = "┆" },
-		},
-		signs_staged = {
-			add = { text = "󱔲 " },
-			change = { text = "󱔲 " },
-			delete = { text = "_" },
-			topdelete = { text = "‾" },
-			changedelete = { text = "~" },
-			untracked = { text = "┆" },
-		},
 		attach_to_untracked = true,
 		on_attach = function(bufnr)
 			local gs = package.loaded.gitsigns
